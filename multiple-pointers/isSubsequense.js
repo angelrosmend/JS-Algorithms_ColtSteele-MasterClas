68ca6f19dc4dac0bd65a94f3500757757b6d46d6
angelrosmend <angelrosmend@gmail.com>
2020-09-08 23:46:36 -0300

    multiple pointers patter isSubsequence

--- multiple-pointers/isSubsequense.js
+++ multiple-pointers/isSubsequense.js
@@ -1,3 +1,24 @@
+         // i
+let str1 = "abc"
+        //  j
+var str2 = "abracadabra"
+
+
 function isSubsequense(str1, str2){
-    var strA = str1.split('').length
+    let sequense = []
+    var i = 0;
+    while(i < str1.length){
+        for(var j = 0; j < str2.length; j++){
+            if(str1[i] == str2[j]){
+                i++
+                sequense.push(str2[j])
+                //console.log(i + 'i')
+            }
+            //console.log(j + 'j')
+        }
+       return sequense.join('') === str1 ?  true : false;
+       // else return false
+    }
 }
+
+console.log(isSubsequense(str1, str2))
